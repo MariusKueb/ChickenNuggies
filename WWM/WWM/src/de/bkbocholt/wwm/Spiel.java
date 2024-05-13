4a27efdca3db5d3ea573f6c55e2e0b7c1ce50d85
flodoeing <flodoeing@gmail.com>
2024-05-13 10:24:29 +0200

    kjhlkh
    jhgkjg

--- WWM/WWM/src/de/bkbocholt/wwm/Spiel.java
+++ WWM/WWM/src/de/bkbocholt/wwm/Spiel.java
@@ -3,7 +3,7 @@ package de.bkbocholt.wwm;
 public class Spiel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Sysem.out.println("Wer wird Millionaer!!!");
 
 	}
 
